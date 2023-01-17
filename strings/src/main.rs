8cad5b49fb8c01b62596d2884956b639540d2298
andri-yta <andriani.yunita@futurice.com>
2023-01-17 10:36:29 +0200

    Add strings exercise

---
+++ strings/src/main.rs
@@ -0,0 +1,63 @@
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let paths =  prefix.split("/");
+    let mut req_paths = request_path.split("/");
+    
+    for path in paths {
+        let req_path = match req_paths.nth(0) {
+            Some(s) => s,
+            _ => ""
+        };
+
+        if path != "*" {
+            if path != req_path {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
+
+fn main() {
+    prefix_matches("/v1/publishers", "/v1/publishers/abc-123");
+    prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    );
+    prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    );
+}
